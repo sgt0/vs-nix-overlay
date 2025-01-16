88c7d63143a20e232dd08552fefa1ab99afc9d55
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-01-16 17:50:59 +0000

    flake.nix: refactor to pass `nix flake check`

--- flake.nix
+++ flake.nix
@@ -1,19 +1,41 @@
 {
   description = "vs-nix-overlay";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = {nixpkgs, ...}: let
-    systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-  in {
-    packages = forAllSystems (system:
-      import ./default.nix {
-        pkgs = import nixpkgs {inherit system;};
-      });
-
-    devShells = forAllSystems (pkgs: {default = import ./shell.nix {inherit pkgs;};});
-
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+    outputs = flake-utils.lib.eachSystem systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) (import ./default.nix {inherit pkgs;});
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          alejandra
+          nil
+          nixd
+        ];
+      };
+
+      formatter = pkgs.alejandra;
+    });
+  in
+    outputs
+    // {
+      overlays.default = final: prev: {
+        vs-nix-overlay = outputs.packages.${prev.system};
+      };
+    };
 }
