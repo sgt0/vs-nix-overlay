5ad4bc47293aedaff21f10790e34e09d2861f03e
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-01-16 17:50:59 +0000

    flake.nix: refactor to pass `nix flake check`

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
-{pkgs ? import <nixpkgs> {}}: {
-  default = pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [
-      alejandra
-      nil
-      nixd
-    ];
-  };
-}
+(import
+  (
+    let
+      flake-compat = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.flake-compat;
+    in
+      fetchTarball {
+        url = "https://github.com/edolstra/flake-compat/archive/${flake-compat.locked.rev}.tar.gz";
+        sha256 = flake-compat.locked.narHash;
+      }
+  )
+  {src = ./.;})
+.shellNix
