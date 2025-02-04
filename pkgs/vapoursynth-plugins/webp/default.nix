77dd18ce86cc927a87818cf0e6045d940a445194
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-02-04 17:16:21 +0000

    webp: 1.0.4 -> 1.0.5

--- pkgs/vapoursynth-plugins/webp/default.nix
+++ pkgs/vapoursynth-plugins/webp/default.nix
@@ -6,16 +6,16 @@
 rustPlatform.buildRustPackage rec {
   pname = "webp";
   # renovate: datasource=github-releases depName=sgt0/vapoursynth-webp
-  version = "1.0.4";
+  version = "1.0.5";
 
   src = fetchFromGitHub {
     owner = "sgt0";
     repo = "vapoursynth-webp";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Jx7/RBmyOYrxNpMBBBB0hlpFD4JCSCPMdG9uoymyjso=";
+    hash = "sha256-zAYHj8F4A5r0BfATa8oEsouj9mBe8WIAuXxCdH1KXcU=";
   };
 
-  cargoHash = "sha256-rlnAD68LuwoRjGedYFS8lEi/9YKvvPB4mP+9X3XZlyc=";
+  cargoHash = "sha256-qbVeztwIvpurb5+us5rrlAfpFkr6Zrlq8NYI+hjjDA0=";
 
   postInstall = ''
     mkdir $out/lib/vapoursynth
