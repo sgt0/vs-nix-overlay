78a6554752a5e73be680926c36dd30526a51e9b9
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-02-08 16:22:38 +0000

    vszip: 3 -> 5

--- pkgs/vapoursynth-plugins/vszip/default.nix
+++ pkgs/vapoursynth-plugins/vszip/default.nix
@@ -4,40 +4,45 @@
   fetchFromGitHub,
   zig, # Requires nightly.
   optimizeLevel ? "ReleaseFast",
-}:
-stdenv.mkDerivation rec {
-  pname = "vszip";
-  # renovate: datasource=github-releases depName=dnjulek/vapoursynth-zip extractVersion=^R(?<version>.+)$
-  version = "3";
-
-  src = fetchFromGitHub {
-    owner = "dnjulek";
-    repo = "vapoursynth-zip";
-    rev = "refs/tags/R${version}";
-    hash = "sha256-YwC78Yw4r3TJg9CKgVuNlF7/5tratnoKMr9Rgfz/60k=";
+}: let
+  zig_hook = zig.hook.overrideAttrs {
+    zig_default_flags = "-Dcpu=baseline -Doptimize=${optimizeLevel}";
   };
+in
+  stdenv.mkDerivation rec {
+    pname = "vszip";
+    # renovate: datasource=github-releases depName=dnjulek/vapoursynth-zip extractVersion=^R(?<version>.+)$
+    version = "5";
 
-  nativeBuildInputs = [
-    zig
-  ];
+    src = fetchFromGitHub {
+      owner = "dnjulek";
+      repo = "vapoursynth-zip";
+      rev = "refs/tags/R${version}";
+      hash = "sha256-1mmM8LYcOGQQZyi13SdEl1pTFjKLB9IQUllcH1Dje/k=";
+    };
 
-  dontConfigure = true;
+    nativeBuildInputs = [
+      zig_hook
+    ];
 
-  preBuild = ''
-    # Necessary for zig cache to work.
-    export HOME=$TMPDIR
-  '';
+    dontConfigure = true;
 
-  installPhase = ''
-    runHook preInstall
-    zig build -Doptimize=${optimizeLevel} --prefix $out/vapoursynth install
-    runHook postInstall
-  '';
+    preBuild = ''
+      # Necessary for zig cache to work.
+      export HOME=$TMPDIR
+    '';
 
-  meta = with lib; {
-    description = "VapourSynth Zig Image Process";
-    homepage = "https://github.com/dnjulek/vapoursynth-zip";
-    license = licenses.mit;
-    platforms = platforms.all;
-  };
-}
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/lib/vapoursynth
+      ln -s zig-out/lib/libvszip${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/vapoursynth/libvszip${stdenv.hostPlatform.extensions.sharedLibrary}
+      runHook postInstall
+    '';
+
+    meta = with lib; {
+      description = "VapourSynth Zig Image Process";
+      homepage = "https://github.com/dnjulek/vapoursynth-zip";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  }
