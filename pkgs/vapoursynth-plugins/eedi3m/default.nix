5030ccda5b7e043c4435b34b1b592194a64eb954
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-02-13 17:45:36 +0000

    eedi3m: init at 4-unstable-2025-02-13

---
+++ pkgs/vapoursynth-plugins/eedi3m/default.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  boost183,
+  opencl-headers,
+  ocl-icd,
+  vapoursynth,
+}:
+stdenv.mkDerivation {
+  pname = "eedi3m";
+  version = "4-unstable-2025-02-13";
+
+  src = fetchFromGitHub {
+    owner = "HomeOfVapourSynthEvolution";
+    repo = "VapourSynth-EEDI3";
+    rev = "d11bdb37c7a7118cd095b53d9f8fbbac02a06ac0";
+    hash = "sha256-MIUf6sOnJ2uqGw3ixEHy1ijzlLFkQauwtm1vfgmYmcg=";
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    meson
+    ninja
+  ];
+
+  buildInputs = [
+    boost183
+    opencl-headers
+    ocl-icd
+    vapoursynth
+  ];
+
+  postPatch = ''
+    substituteInPlace meson.build \
+      --replace-fail "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
+  '';
+
+  mesonBuildType = "release";
+
+  meta = with lib; {
+    description = "An intra-field only deinterlacer";
+    homepage = "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-EEDI3";
+    license = licenses.gpl2;
+    platforms = platforms.all;
+  };
+}
