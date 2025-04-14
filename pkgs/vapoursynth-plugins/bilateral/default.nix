4c08930a030b2c59bfca83dae6ee6ac377adcd0b
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-04-14 22:41:14 +0000

    bilateral: mark as broken with clang

--- pkgs/vapoursynth-plugins/bilateral/default.nix
+++ pkgs/vapoursynth-plugins/bilateral/default.nix
@@ -5,36 +5,41 @@
   vapoursynth,
   pkg-config,
   which,
-}:
-stdenv.mkDerivation {
-  pname = "bilateral";
-  version = "3-unstable-2025-01-19";
+}: let
+  inherit (lib) optionals;
+in
+  stdenv.mkDerivation {
+    pname = "bilateral";
+    version = "3-unstable-2025-01-19";
 
-  src = fetchFromGitHub {
-    owner = "HomeOfVapourSynthEvolution";
-    repo = "VapourSynth-Bilateral";
-    rev = "5c246c08914661fa2711eba5b7e8ca383bf0b717";
-    hash = "sha256-whnL4U+kwv0m9jWkXIUYhnZ2BAn05IugfBZj9lGupyI=";
-  };
+    src = fetchFromGitHub {
+      owner = "HomeOfVapourSynthEvolution";
+      repo = "VapourSynth-Bilateral";
+      rev = "5c246c08914661fa2711eba5b7e8ca383bf0b717";
+      hash = "sha256-whnL4U+kwv0m9jWkXIUYhnZ2BAn05IugfBZj9lGupyI=";
+    };
 
-  makefile = "GNUmakefile";
+    makefile = "GNUmakefile";
 
-  nativeBuildInputs = [
-    pkg-config
-    which
-  ];
+    nativeBuildInputs = [
+      pkg-config
+      which
+    ];
 
-  buildInputs = [
-    vapoursynth
-  ];
+    buildInputs = [
+      vapoursynth
+    ];
 
-  dontAddPrefix = true;
-  configureFlags = ["--install=$(out)/lib/vapoursynth"];
+    dontAddPrefix = true;
+    configureFlags =
+      ["--install=$(out)/lib/vapoursynth"]
+      ++ optionals stdenv.cc.isClang ["--cxx=${stdenv.cc.targetPrefix}clang++"];
 
-  meta = with lib; {
-    description = "Bilateral filter for VapourSynth.";
-    homepage = "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Bilateral";
-    license = licenses.gpl3;
-    platforms = platforms.all;
-  };
-}
+    meta = with lib; {
+      description = "Bilateral filter for VapourSynth.";
+      homepage = "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Bilateral";
+      license = licenses.gpl3;
+      platforms = platforms.all;
+      broken = stdenv.cc.isClang; # configure script uses g++ arguments.
+    };
+  }
