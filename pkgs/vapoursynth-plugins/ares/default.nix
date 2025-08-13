5d81b61dfa5919abb6b8eb428395edd9b1817fbe
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-08-13 16:52:53 +0000

    ares: 0.1.1 -> 0.2.0

--- pkgs/vapoursynth-plugins/ares/default.nix
+++ pkgs/vapoursynth-plugins/ares/default.nix
@@ -9,13 +9,13 @@
 stdenv.mkDerivation rec {
   pname = "ares";
   # renovate: datasource=github-tags depName=ropagr/VapourSynth-AudioResample
-  version = "0.1.1";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "ropagr";
     repo = "VapourSynth-AudioResample";
     rev = "refs/tags/v${version}";
-    hash = "sha256-MLKWl/1DqUypN6gSGY571AGSOQjUeS+skLOXRPgmGfI=";
+    hash = "sha256-89lNPBaWg3ubbMdKnGTj0pwSrpTH+0o/7N8fRD/hbZw=";
   };
 
   patches = [
