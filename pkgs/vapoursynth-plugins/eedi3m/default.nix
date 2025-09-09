7667386fa4dcba7dfc0821b47531d67344ddc1f5
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-09-09 21:35:06 +0000

    eedi3m: 4-unstable-2025-02-13 -> 6

--- pkgs/vapoursynth-plugins/eedi3m/default.nix
+++ pkgs/vapoursynth-plugins/eedi3m/default.nix
@@ -5,48 +5,38 @@
   meson,
   ninja,
   pkg-config,
-  vapoursynth,
-  withOpenCL ? false,
   boost,
-  ocl-icd,
-  opencl-headers,
+  vapoursynth,
 }:
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "eedi3m";
-  version = "4-unstable-2025-02-13";
+  # renovate: datasource=github-releases depName=HomeOfVapourSynthEvolution/VapourSynth-EEDI3 extractVersion=^r(?<version>.+)$
+  version = "6";
 
   src = fetchFromGitHub {
     owner = "HomeOfVapourSynthEvolution";
     repo = "VapourSynth-EEDI3";
-    rev = "d11bdb37c7a7118cd095b53d9f8fbbac02a06ac0";
-    hash = "sha256-MIUf6sOnJ2uqGw3ixEHy1ijzlLFkQauwtm1vfgmYmcg=";
+    rev = "refs/tags/r${version}";
+    hash = "sha256-F6BxS653OeLr4XJE47xEWmhFgS4GkLqDP+Jv7oj66Go=";
   };
 
   nativeBuildInputs = [
-    pkg-config
     meson
     ninja
+    pkg-config
   ];
 
-  buildInputs =
-    [
-      boost
-      vapoursynth
-    ]
-    ++ lib.optional withOpenCL [
-      ocl-icd
-      opencl-headers
-    ];
+  buildInputs = [
+    boost
+    vapoursynth
+  ];
 
   postPatch = ''
     substituteInPlace meson.build \
-      --replace-fail "vapoursynth_dep.get_pkgconfig_variable('libdir')" "get_option('libdir')"
+      --replace-fail "vapoursynth_dep.get_variable(pkgconfig: 'libdir')" "get_option('libdir')"
   '';
 
   mesonBuildType = "release";
-  mesonFlags = [
-    (lib.mesonBool "opencl" withOpenCL)
-  ];
 
   meta = with lib; {
     description = "An intra-field only deinterlacer";
