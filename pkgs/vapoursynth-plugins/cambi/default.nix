daff2b9f40e746297be6d9026093a4e2ad8c26f4
sgt0 <140186177+sgt0@users.noreply.github.com>
2025-12-23 01:01:56 +0000

    cambi: 1.1.3 -> 1.1.4

--- pkgs/vapoursynth-plugins/cambi/default.nix
+++ pkgs/vapoursynth-plugins/cambi/default.nix
@@ -7,16 +7,16 @@
 rustPlatform.buildRustPackage rec {
   pname = "cambi";
   # renovate: datasource=github-releases depName=sgt0/vapoursynth-cambi
-  version = "1.1.3";
+  version = "1.1.4";
 
   src = fetchFromGitHub {
     owner = "sgt0";
     repo = "vapoursynth-cambi";
     rev = "refs/tags/v${version}";
-    hash = "sha256-n6RE1Kz/4P+vu6xRhUYtHS+n0C1H9saguNkeMUa1WhI=";
+    hash = "sha256-f30uqW7QYOw6p+HZk23SVvGBhG1cohHl0dy5HZ3Azwk=";
   };
 
-  cargoHash = "sha256-XWXeNVFr3JDHiETnR7jP/S8bYNUPEAqTOOwPxC2EWOs=";
+  cargoHash = "sha256-jRuQkjGrQQfJZ56ca9a5sjvexeDNArLDNyeyE7qAROs=";
 
   postInstall = ''
     mkdir $out/lib/vapoursynth
