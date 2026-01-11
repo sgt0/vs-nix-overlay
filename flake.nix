6b2425b033653533fd569f2efa9e494157867aed
sgt0 <140186177+sgt0@users.noreply.github.com>
2026-01-11 23:27:35 +0000

    flake: follow cachix/devenv-nixpkgs/rolling

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "vs-nix-overlay";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat.url = "github:edolstra/flake-compat";
   };
