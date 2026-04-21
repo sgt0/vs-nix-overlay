cac68fa6f22b839b89243399daf9edaf287532a2
sgt0 <140186177+sgt0@users.noreply.github.com>
2026-04-21 02:15:04 +0000

    eedi3m: 8 -> 9

--- pkgs/vapoursynth-plugins/eedi3m/default.nix
+++ pkgs/vapoursynth-plugins/eedi3m/default.nix
@@ -11,13 +11,13 @@
 stdenv.mkDerivation rec {
   pname = "eedi3m";
   # renovate: datasource=github-releases depName=HomeOfVapourSynthEvolution/VapourSynth-EEDI3 extractVersion=^r(?<version>.+)$
-  version = "8";
+  version = "9";
 
   src = fetchFromGitHub {
     owner = "HomeOfVapourSynthEvolution";
     repo = "VapourSynth-EEDI3";
     rev = "refs/tags/r${version}";
-    hash = "sha256-QSysnsoFYlgbik7AUQIwos4vClVRvFh9gZYu2lPREa0=";
+    hash = "sha256-/3elqMGarp1+T7K0wOIEbePsa80UUhMEwnYUudNnGxg=";
   };
 
   nativeBuildInputs = [
@@ -32,8 +32,10 @@ stdenv.mkDerivation rec {
   ];
 
   postPatch = ''
+    sed -i "/^incdir = /,/^)/c\incdir = include_directories('${vapoursynth}/include/vapoursynth')" meson.build
     substituteInPlace meson.build \
-      --replace-fail "vapoursynth_dep.get_variable('libdir')" "get_option('libdir')"
+      --replace-fail "import('python').find_installation(pure: false)" "disabler()" \
+      --replace-fail "py.get_install_dir() / 'vapoursynth/plugins'" "get_option('libdir') / 'vapoursynth'"
   '';
 
   mesonBuildType = "release";
