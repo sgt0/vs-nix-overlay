a0a9ec09d6db77addc2995789930d48c712acd21
sgt0 <140186177+sgt0@users.noreply.github.com>
2026-04-28 13:09:54 +0000

    placebo: 2.0.0 -> 2.0.2

--- pkgs/vapoursynth-plugins/placebo/default.nix
+++ pkgs/vapoursynth-plugins/placebo/default.nix
@@ -2,49 +2,60 @@
   lib,
   stdenv,
   fetchFromGitHub,
+  fetchurl,
   meson,
   ninja,
   pkg-config,
   libplacebo,
   vapoursynth,
   vulkan-headers,
-}:
-stdenv.mkDerivation rec {
-  pname = "placebo";
-  # renovate: datasource=github-releases depName=Lypheo/vs-placebo
-  version = "2.0.0";
-
-  src = fetchFromGitHub {
-    owner = "Lypheo";
-    repo = "vs-placebo";
-    rev = "refs/tags/${version}";
-    hash = "sha256-t+Gw33dDeVMJvDT+1EnYvfRl07MHuYXx0YMitisWIr4=";
-    fetchSubmodules = true;
+}: let
+  libp2pRev = "f50288b0c8db2cb14bb98fc25a5f056609d03652";
+  libp2p = fetchurl {
+    url = "https://github.com/sekrit-twc/libp2p/archive/${libp2pRev}.tar.gz";
+    hash = "sha256-N7FL5bEQgmjlWqT7r4OMKHAY7MW1jhXv3+BkkWr8ROk=";
   };
+in
+  stdenv.mkDerivation rec {
+    pname = "placebo";
+    # renovate: datasource=github-releases depName=Lypheo/vs-placebo
+    version = "2.0.2";
 
-  nativeBuildInputs = [
-    pkg-config
-    meson
-    ninja
-  ];
+    src = fetchFromGitHub {
+      owner = "Lypheo";
+      repo = "vs-placebo";
+      rev = "refs/tags/${version}";
+      hash = "sha256-08BGiEQ5mVQhyH/erFFLvEzqNSz0kHyhe/BLXspae1k=";
+    };
 
-  buildInputs = [
-    libplacebo
-    vapoursynth
-    vulkan-headers
-  ];
+    nativeBuildInputs = [
+      pkg-config
+      meson
+      ninja
+    ];
 
-  postPatch = ''
-    substituteInPlace meson.build \
-      --replace-fail "vapoursynth_dep.get_variable(pkgconfig: 'libdir')" "get_option('libdir')"
-  '';
+    buildInputs = [
+      libplacebo
+      vapoursynth
+      vulkan-headers
+    ];
 
-  mesonBuildType = "release";
+    postPatch = ''
+      mkdir -p subprojects/libp2p-${libp2pRev}
+      tar -xzf ${libp2p} --strip-components=1 -C subprojects/libp2p-${libp2pRev}
+      cp subprojects/packagefiles/libp2p-${libp2pRev}/* subprojects/libp2p-${libp2pRev}/
 
-  meta = with lib; {
-    description = "libplacebo-based debanding, scaling and color mapping plugin for VapourSynth";
-    homepage = "https://github.com/Lypheo/vs-placebo";
-    license = licenses.lgpl21;
-    platforms = platforms.all;
-  };
-}
+      substituteInPlace meson.build \
+        --replace-fail "vapoursynth_dep.get_variable(pkgconfig: 'libdir')" "get_option('libdir')"
+    '';
+
+    mesonFlags = ["-Dr73-compat=true"];
+    mesonBuildType = "release";
+
+    meta = with lib; {
+      description = "libplacebo-based debanding, scaling and color mapping plugin for VapourSynth";
+      homepage = "https://github.com/Lypheo/vs-placebo";
+      license = licenses.lgpl21;
+      platforms = platforms.all;
+    };
+  }
