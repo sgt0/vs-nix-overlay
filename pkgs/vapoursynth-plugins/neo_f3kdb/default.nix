544eab97cd0ea5f86701a08f955eb99e5898c2e1
sgt0 <140186177+sgt0@users.noreply.github.com>
2026-04-28 20:05:00 +0000

    neo_f3kdb: 9-unstable-2025-04-19 -> 10

--- pkgs/vapoursynth-plugins/neo_f3kdb/default.nix
+++ pkgs/vapoursynth-plugins/neo_f3kdb/default.nix
@@ -9,18 +9,15 @@
 }:
 stdenv.mkDerivation rec {
   pname = "neo_f3kdb";
-  version = "9-unstable-2025-04-19";
+  # renovate: datasource=github-tags depName=HomeOfAviSynthPlusEvolution/neo_f3kdb extractVersion=^r(?<version>.+)$
+  version = "10";
 
   src = fetchFromGitHub {
     owner = "HomeOfAviSynthPlusEvolution";
     repo = "neo_f3kdb";
-    rev = "ad9fa1a11412ab46199d3b8e7cc2e5a89f1d5d1a";
-    hash = "sha256-bLBSO3x51A9RR7d21Z8S0vL4ZDALKO0epI+3WlTneKU=";
+    rev = "refs/tags/r${version}";
+    hash = "sha256-9aEJkK/5ObHJtPqf6CyB0JuqZbXvjZQArfgs+ChAt20=";
   };
-
-  patches = [
-    ./no-git.patch
-  ];
 
   nativeBuildInputs = [
     cmake
@@ -45,6 +42,6 @@ stdenv.mkDerivation rec {
     description = "Basically like f3kdb but with legacy format support removed (in Avisynth) and with new sample modes.";
     homepage = "https://github.com/HomeOfAviSynthPlusEvolution/neo_f3kdb";
     license = licenses.gpl3;
-    platforms = platforms.all;
+    platforms = platforms.x86;
   };
 }
