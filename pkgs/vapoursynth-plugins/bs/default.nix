7067d0e7831251bcd681ffff74a948f0e85a4270
sgt0 <140186177+sgt0@users.noreply.github.com>
2026-07-09 13:14:57 +0000

    bs: 18 -> 19

--- pkgs/vapoursynth-plugins/bs/default.nix
+++ pkgs/vapoursynth-plugins/bs/default.nix
@@ -6,70 +6,80 @@
   meson,
   ninja,
   pkg-config,
+  python3,
   vapoursynth,
   xxhash,
-}:
-stdenv.mkDerivation rec {
-  pname = "bs";
-  # renovate: datasource=github-releases depName=vapoursynth/bestsource extractVersion=^R(?<version>.+)$
-  version = "18";
-
-  outputs = [
-    "out"
-    "dev"
-  ];
-
-  src = fetchFromGitHub {
-    owner = "vapoursynth";
-    repo = "bestsource";
-    rev = "refs/tags/R${version}";
-    hash = "sha256-tF0rJT1cH6p71RP4w0If9lP1ZVRIWKoIyxEg1/rscbg=";
-    fetchSubmodules = true;
+}: let
+  libp2p = fetchFromGitHub {
+    owner = "sekrit-twc";
+    repo = "libp2p";
+    rev = "869fa993041f9f3af7d9ac8b10158920c6ddce66";
+    hash = "sha256-84mCTO8MFWAcQ5WVC5W5w1a7caBQnMZ3pfRF6PbuSZg=";
   };
-
-  nativeBuildInputs = [
-    pkg-config
-    meson
-    ninja
-  ];
-
-  buildInputs = [
-    ffmpeg.dev
-    vapoursynth
-    xxhash
-  ];
-
-  postPatch = ''
-        substituteInPlace meson.build \
-          --replace-fail \
-            "incdir = include_directories(
-        run_command(
-            find_program('python', 'python3'),
-            '-c',
-            'import vapoursynth as vs; print(vs.get_include())',
-            check: true,
-        ).stdout().strip(),
-        'AviSynthPlus/avs_core/include'
-    )" \
-            "deps += dependency('vapoursynth')
-    incdir = include_directories(
-        'AviSynthPlus/avs_core/include'
-    )"
-  '';
-
-  mesonBuildType = "release";
-
-  postInstall = ''
-    mkdir -p $out/lib/vapoursynth
-    mv $out/lib/python*/site-packages/vapoursynth/plugins/libbestsource${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/vapoursynth/
-    # Clean up the python site-packages tree left behind.
-    rm -rf $out/lib/python*
-  '';
-
-  meta = with lib; {
-    description = "A super great audio/video source and FFmpeg wrapper";
-    homepage = "https://github.com/vapoursynth/bestsource";
-    license = licenses.mit;
-    platforms = platforms.all;
+  avisynthplus = fetchFromGitHub {
+    owner = "AviSynth";
+    repo = "AviSynthPlus";
+    rev = "v3.7.5";
+    hash = "sha256-RkEZWsAKZABtl+SbRLCjMqyQoi9ainbaI9hWlpO6Fwo=";
   };
-}
+in
+  stdenv.mkDerivation rec {
+    pname = "bs";
+    # renovate: datasource=github-releases depName=vapoursynth/bestsource extractVersion=^R(?<version>.+)$
+    version = "19";
+
+    outputs = [
+      "out"
+      "dev"
+    ];
+
+    src = fetchFromGitHub {
+      owner = "vapoursynth";
+      repo = "bestsource";
+      rev = "refs/tags/R${version}";
+      hash = "sha256-qkbh6hKq2Q93Y54ZZe9AG/FhuLt5CEX1iYG3wkXDx78=";
+    };
+
+    nativeBuildInputs = [
+      pkg-config
+      meson
+      ninja
+      python3
+    ];
+
+    buildInputs = [
+      ffmpeg.dev
+      vapoursynth
+      xxhash
+    ];
+
+    postPatch = ''
+      cp -r --no-preserve=mode ${libp2p} subprojects/libp2p
+      cp -r --no-preserve=mode ${avisynthplus} subprojects/avisynthplus
+      cp subprojects/packagefiles/libp2p/meson.build subprojects/libp2p/
+      cp subprojects/packagefiles/avisynthplus/meson.build subprojects/avisynthplus/
+
+      substituteInPlace meson.build \
+        --replace-fail \
+          "incdir += include_directories(
+              run_command(py, '-c', 'import vapoursynth as vs; print(vs.get_include())', check: true).stdout().strip(),
+          )" \
+          "deps += dependency('vapoursynth')"
+    '';
+
+    mesonBuildType = "release";
+
+    postInstall = ''
+      mkdir -p $out/lib/vapoursynth
+      mv $out/lib/python*/site-packages/vapoursynth/plugins/libbestsource${stdenv.hostPlatform.extensions.sharedLibrary} $out/lib/vapoursynth/
+      # Clean up the python site-packages tree left behind.
+      rm -rf $out/lib/python*
+    '';
+
+    meta = with lib; {
+      description = "A super great audio/video source and FFmpeg wrapper";
+      homepage = "https://github.com/vapoursynth/bestsource";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+  }
